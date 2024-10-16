952df5a7d7c605b82b9c367314f24e38dbda90de
Meshack - <rutomeshack18@gmail.com>
2024-10-16 03:38:35 +0300

    Finalizing

--- Ecommerce_API/Ecommerce_API/settings.py
+++ Ecommerce_API/Ecommerce_API/settings.py
@@ -98,7 +98,10 @@ WSGI_APPLICATION = 'Ecommerce_API.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 # Password validation
